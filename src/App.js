bd898881b0591abc3ed292c612f200f54c9ec36d
Natalie <nataliech0409@gmail.com>
2022-06-13 12:57:58 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './scss/app.scss';
+import Header from './modules/header/Header';
+import Categories from './modules/categories/Categories';
+import Sort from './modules/sort/Sort';
+import PizzaBlock from './modules/pizzaBlock/PizzaBlock';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <div className="content__top">
+                        <Categories/>
+                        <Sort/>
+                    </div>
+                    <h2 className="content__title">Все пиццы</h2>
+                    <div className="content__items">
+                        <PizzaBlock
+                            imageUrl="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+                            price={500}
+                            title="Мексиканская"
+                        />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
